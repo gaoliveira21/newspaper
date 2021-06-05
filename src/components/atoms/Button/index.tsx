89faf26c24bb9ceb9012acdb84fbb00084a9ac54
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2021-06-05 18:20:57 -0300

    feat: change Button styles

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -7,7 +7,7 @@ export function Button({ children, className }: ButtonProps) {
   return (
     <button
       id="btn"
-      className={`px-5 py-2 rounded-none border-2 border-primary uppercase text-primary font-bold hover:bg-primary hover:text-white transition-colors duration-200 ${className}`}
+      className={`px-5 py-2 bg-primary rounded-none uppercase text-white font-bold hover:bg-primary hover:opacity-95 transition-opacity duration-200 ${className}`}
     >
       {children}
     </button>
