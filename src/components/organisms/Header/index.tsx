2d90e0fc66be33a67c3d35334a8033de3aa09499
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2021-06-05 16:48:47 -0300

    feat: add Header component

---
+++ src/components/organisms/Header/index.tsx
@@ -0,0 +1,21 @@
+import { Button, Logo } from '@/components/atoms'
+import { Link } from '@/components/molecules/Link'
+
+export function Header() {
+  return (
+    <header>
+      <div className="py-4 flex justify-center sm:justify-between items-center">
+        <div className="hidden sm:block">
+          <Link href="#">Search</Link>
+        </div>
+
+        <Logo />
+
+        <div className="hidden md:flex items-center">
+          <Link href="#" classNames="mr-4">SIGN IN</Link>
+          <Button>SUBSCRIBE</Button>
+        </div>
+      </div>
+    </header>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,10 @@
-import { Button, Container, Logo, Text } from "@/components/atoms";
-import { Link } from "@/components/molecules/Link";
+import { Container } from "@/components/atoms";
+import { Header } from "@/components/organisms/Header";
 
 export default function Home() {
   return (
     <Container>
-      <Logo />
-      <Button>subscribe</Button>
-      <Text>The director Craig Gillespie discusses the formation of the title chara...</Text>
-      <Link href="#">
-        Link
-      </Link>
+      <Header />
     </Container>
   )
 }
