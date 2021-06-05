9804ab56137d67be15c0c272adc930e1c0c186ab
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2021-06-05 16:44:37 -0300

    feat: add classNames prop

--- src/components/molecules/Link/index.tsx
+++ src/components/molecules/Link/index.tsx
@@ -5,13 +5,14 @@ import { Text } from '@/components/atoms/Text'
 type LinkProps = {
   href: string
   children: React.ReactNode
+  classNames?: string
 }
 
-export function Link({ href, children }: LinkProps) {
+export function Link({ href, children, classNames }: LinkProps) {
   return (
     <NextLink href={href}>
       <Text color="heading" weight="bold">
-        <a className="hover:text-primary cursor-pointer transition-colors duration-100">{children}</a>
+        <a className={`hover:text-primary cursor-pointer transition-colors duration-100 ${classNames}`}>{children}</a>
       </Text>
     </NextLink>
   )
