87aa14bc63322277d0e00222852d436433fd7ddd
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2021-06-05 14:40:52 -0300

    feat: add logo component

---
+++ src/components/atoms/Logo/index.tsx
@@ -0,0 +1,10 @@
+export function Logo () {
+  return (
+    <h1
+      className="font-heading text-heading text-4xl md:text-6xl pl-1 border-l-4 border-primary font-bold"
+    >
+      Newspaper
+      <span className="text-primary">.</span>
+    </h1>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
+import { Logo } from "@/components/atoms/Logo";
+
 export default function Home() {
   return (
-    <h1 className="bg-primary font-heading">Hello</h1>
+    <Logo />
   )
 }
